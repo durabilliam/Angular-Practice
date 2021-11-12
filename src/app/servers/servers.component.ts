da7453bc13cf97295028ee3936bb837a985daa8b
William Perry <durabilliam@gmail.com>
2021-11-12 12:21:21 -0330

    string interpolation and property binding examples

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,20 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',  //usually select by component
   // selector: '[app-servers]',  //selecting by attribute
   // selector: '.app-servers',  //selecting by class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
   }
